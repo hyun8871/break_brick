15e7067de15045fbc0090b4ad75584c54db72a76
hyun8871 <101275354+hyun8871@users.noreply.github.com>
2024-05-21 15:58:40 +0900

    z

--- classes.py
+++ classes.py
@@ -1,17 +1,30 @@
 import pygame
 
 class brick:
-    def __init__(self):
-        pass
+    def __init__(self, x, y, w, h, hp, type):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.hp = hp
+        self.type = type
     def display(self):
         pygame.image.load("brick.png")
-
+    def onBallCollision(self):
+        
 class ball:
     def __init__(self):
         pass
+    def display(self):
+        pygame.image.load("bar.png")
+    def freeMove(self): #매 프레임마다 업데이트 되는 공기중에서의 움직임
+        pass
+    def onCollision(self): #충돌시 반발되는 것
 
 class bar:
-    def __init__(self):
-        pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
     def move(self): #키 입력에 따라서 Bar 위치 이동
         pass
+        
