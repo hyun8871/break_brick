e2f6d2264d9c9e06f7b8a416c76c590670cac947
hyun8871 <101275354+hyun8871@users.noreply.github.com>
2024-05-21 14:54:37 +0900

    기반 작업

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello mm")
+import pygame
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 640
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Collision at boundary")
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+
+pygame.quit()
